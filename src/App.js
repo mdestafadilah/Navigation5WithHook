755267b4eb19eac0d9f965972fddfc145490a3b7
Desta F Muchamad <desta.08b@gmail.com>
2020-09-09 16:28:48 +0700

    Added Auth Context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -6,14 +6,31 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {AuthStackNavigator} from './navigators/AuthStackNavigator';
 import {lightTheme} from './themes/light';
 
+// Context
+import {AuthContext} from './contexts/AuthContext';
+
 const RootStack = createStackNavigator();
 
 export default function () {
+  const auth = useMemo(() => ({
+    login: () => {
+      console.log('login');
+    },
+    logout: () => {
+      console.log('logout');
+    },
+    register: () => {
+      console.log('register');
+    },
+  }));
+
   return (
-    <NavigationContainer theme={lightTheme}>
-      <RootStack.Navigator screenOptions={{headerShown: false}}>
-        <RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={auth}>
+      <NavigationContainer theme={lightTheme}>
+        <RootStack.Navigator screenOptions={{headerShown: false}}>
+          <RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
